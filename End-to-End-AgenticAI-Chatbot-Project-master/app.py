a497a14a57b8a4ef424fa6fdc88c26f049494af9
chandraprabha Arun <cprabha@hotmail.com>
2025-06-05 08:00:58 +0400

    Initial commit

---
+++ End-to-End-AgenticAI-Chatbot-Project-master/app.py
@@ -0,0 +1,4 @@
+from src.langgraphagenticai.main import load_langgraph_agenticai_app
+# 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
+if __name__=="__main__": 
+    load_langgraph_agenticai_app()
